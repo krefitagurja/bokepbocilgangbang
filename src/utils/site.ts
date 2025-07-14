e6286337893e35a220ab857d4474601395d2552d
krefitagurja <krefitagurja@gmail.com>
2025-07-14 11:15:06 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Bocil Gangbang';
-export const desk = 'Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma';
-export const url = 'https://bokepbocilgangbang.pages.dev';
-export const terbit = '2025-06-26T00:01:03Z';
